b365206bbe669c1478dd597db82848517cedbe0b
mynuni <elelaka0000@gmail.com>
2023-10-22 00:03:41 +0900

    Add test component

---
+++ src/components/TestComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TestComponent = () => {
+    return (
+        <div>
+            THIS IS A TEST COMPONENT.
+        </div>
+    );
+};
+
+export default TestComponent;
