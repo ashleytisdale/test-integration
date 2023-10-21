fb852e2f181c261e5e839d045317aa7f209375f7
mynuni <elelaka0000@gmail.com>
2023-10-22 00:08:26 +0900

    Update controller

--- src/main/java/com/example/gittestbackend/controller/SecondTestController.java
+++ src/main/java/com/example/gittestbackend/controller/SecondTestController.java
@@ -1,4 +1,14 @@
 package com.example.gittestbackend.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class SecondTestController {
+
+    @GetMapping("/hello")
+    public void hello() {
+        System.out.println("HELLO WORLD");
+    }
+
 }
--- src/main/java/com/example/gittestbackend/controller/TestController.java
+++ src/main/java/com/example/gittestbackend/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.example.gittestbackend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,7 +9,12 @@ public class TestController {
 
     @GetMapping
     public void test() {
-        // THIS IS A TEST CONTROLLER.
+        // Test controller has been modified
+    }
+
+    @PostMapping
+    public void post() {
+
     }
 
 }
